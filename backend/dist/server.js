7f1b6be1b4f674fd9c393a54caa31e37f7fb4381
Edilson Mutisse <98753172+Mutisse@users.noreply.github.com>
2025-11-14 11:59:32 +0200

    Prepare for deployment

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -17,6 +17,13 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)()); // Permite requisições de qualquer origem
 app.use(express_1.default.json()); // Para interpretar JSON nas requisições
 app.use("/api", tarefasroutes_1.default); // Prefixar as rotas com "/api"
+// No seu server.ts
+app.use((0, cors_1.default)({
+    origin: [
+        "https://tudolistfrontend.netlify.app/" // Produção
+    ],
+    credentials: true
+}));
 // Iniciar o servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
