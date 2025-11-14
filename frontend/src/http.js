df5a663fa4f87c99b3e4aa05259a4c03d2d92bdc
Edilson Mutisse <98753172+Mutisse@users.noreply.github.com>
2025-11-14 10:16:55 +0200

    Prepare for deployment

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Crie uma instância do axios com uma configuração base
 const instance = axios.create({
-  baseURL: "http://localhost:9000/api", // URL base para suas requisições
+  baseURL: "https://to-do-list-fullstak.onrender.com", // URL base para suas requisições
   timeout: 1000, // Tempo de espera para resposta
   headers: { "Content-Type": "application/json" },
 });
